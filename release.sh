7471815d64e80e746b92eacbdf0fb8f1ddab4b84
Mercer Traieste <mercer.traieste@gmail.com>
2015-04-05 13:29:19 +0200

    added list of presentation in home page, added release script to gh-pages

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    echo Usage: `basename $0` GH_PAGES_REPO_PATH 1>&2
+    exit 1
+fi
+
+GH_PAGES=$1
+MASTER="$( cd "$( dirname "$0" )" && pwd )"
+
+cd ${GH_PAGES}
+git rm -rf *
+cp -rf ${MASTER}/* ${GH_PAGES}
+git add -A
+git commit -m "update to new version"
+git push
